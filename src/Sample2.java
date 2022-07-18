e93f472133ef8608f4b1ee58e935e4188ca9dfb9
Aniket S <Aniket S@DESKTOP-NACNKAS>
2022-07-18 20:12:36 +0530

    [Ashwini]: hi everyone

---
+++ src/Sample2.java
@@ -0,0 +1,13 @@
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hiiiii");
+		System.out.println("hiiiii");
+		System.out.println("hiiiii");
+		System.out.println("hiiiii");
+
+	}
+
+}
